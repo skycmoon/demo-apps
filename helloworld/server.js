cc9220e8f80a914c33098775c2047f483055ebb1
Sky <cmoon@expediagroup.com>
2019-09-18 10:14:07 -0700

    use json to represent status.

--- helloworld/server.js
+++ helloworld/server.js
@@ -6,8 +6,11 @@ const Prometheus = require('prom-client');
 // Constants
 const PORT = 8080;
 const HOST = '0.0.0.0';
-const HOSTNAME = process.env.HOSTNAME;
 const VERSION = process.env.VERSION;
+const HOSTNAME = process.env.HOSTNAME;
+const CLUSTER = process.env.CLUSTER;
+const ENVIRONMENT = process.env.ENVIRONMENT;
+const REGION = process.env.REGION;
 
 app.engine('handlebars', exphbs());
 app.set('view engine', 'handlebars');
@@ -25,11 +28,26 @@ app.use((req, res, next) => {
   next()
 })
 
-app.get('/', (req, res) => {
-  res.render('index', {hostname: HOSTNAME, version: VERSION});
+app.get('/app', (req, res) => {
+  res.json({
+    version: VERSION,
+    hostname: HOSTNAME,
+    cluster: CLUSTER,
+    environment: ENVIRONMENT,
+    region: REGION
+  })
 });
 
-app.get('/metrics', (req, res) => {
+// app.get('/app', (req, res) => {
+//   res.render('index', {
+//     hostname: HOSTNAME,
+//     version: VERSION,
+//     environment: ENVIRONMENT,
+//     region: REGION
+//   });
+// });
+
+app.get('/app/metrics', (req, res) => {
   res.set('Content-Type', Prometheus.register.contentType);
   res.end(Prometheus.register.metrics());
 });
